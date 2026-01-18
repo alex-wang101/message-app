6d7b68479fbf5d98220a53d8c74e722a7b4964bc
alex-wang101 <wangalex0410@gmail.com>
2026-01-18 17:27:04 -0500

    frontend?

--- messaging/typing_test.py
+++ messaging/typing_test.py
@@ -3,20 +3,24 @@ import time
 
 class TypingDetector:
     """
-    TODO: The current listener does not detect when a key is held down. 
+    Cool working script, but need to detect whether the user is typing rather than 
     """
     def __init__(self):
-        self.pressed_keys = set()
+        self.pressed_keys = {}
         self.listener = keyboard.Listener(
             on_press=self.on_press,
             on_release=self.on_release)
 
     def on_press(self, key):
-        if key not in self.
+        if key not in self.pressed_keys:
+            self.pressed_keys[key] = time.time()
+            print(f'{key} is pressed')
     
     def on_release(self, key):
-        print('{0} released'.format(
-            key))
+        if key in self.pressed_keys:
+            timer = time.time() - self.pressed_keys[key]
+            del self.pressed_keys[key]
+            print(f'{key} is released, held for {timer} number of seconds')
         if key == keyboard.Key.esc:
             return False
 
